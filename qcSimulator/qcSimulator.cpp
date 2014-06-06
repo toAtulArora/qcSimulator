ba57c40509579374d7968d9ec5cdd5b7a2b9d1e5
Atul Sinh Aurora <toatularora@gmail.com>
2014-06-06 12:04:22 +0530

    Initializing qbits seems to be functional now

--- qcSimulator/qcSimulator.cpp
+++ qcSimulator/qcSimulator.cpp
@@ -23,19 +23,42 @@ public:
 	//tBit=0
 	//value=1
 	//returned=0x1101001
+	//string printNumFancy(int n)
+	//{
+		//return to_string((bitset<8>) n) + " [" + to_string(n) + "]";
+	//}
+
+	string printNumFancy(int n)
+	{
+		stringstream temp;
+		temp<<n<<" ["<<(bitset<8>) n << "]";
+		return temp.str();
+	}
+
 	int insertBit(int num, int tBit, int value)
 	{
+		//USEFUL TEST CODE (USE IT OUTSIDE AND MAKE THIS PUBLIC FIRST)
+		//bitset<8> n(string("00001110"));
+		//cout<<n.to_ullong()<<endl;
+		//int n=0xF;
+		//int nDash=qc.insertBit(n.to_ulong(),0,1);
+		//cout<<"So we had\n"<<n<<endl<<(bitset<8>) nDash<<endl<<"Did that work?";
+		//cout<<"This works";
+
+
 		//int numLHS=num/(int)pow(2,tBit);
-		int numLHS=num>>tBit;
+		int numLHS=num>>tBit; //Calculate quotient
 		//numLHS=numLHS*2*pow(2,tBit);
-		numLHS=numLHS<<(tBit+1);
-		int numRHS = num &((1<<(tBit)) - 1);
+		numLHS=numLHS<<(tBit+1); //Bit shift to proper place
+		int numRHS = num &((1<<(tBit)) - 1); //Calculate remainder
 		//int numRHS=num%(int)pow(2,tBit);
 		//int numRHS=num<<
 		
-		cout<<"Num:"<<(bitset<8>) num<<endl;
-		cout<<"LHS:"<<(bitset<8>) numLHS<<endl;
-		cout<<"RHS:"<<(bitset<8>) numRHS<<endl;
+		//YOU CAN ENABLE THIS FOR DEBUGGING
+		//cout<<"Num:"<<(bitset<8>) num<<endl;
+		//cout<<"LHS:"<<(bitset<8>) numLHS<<endl;
+		//cout<<"RHS:"<<(bitset<8>) numRHS<<endl;
+		
 		//return numRHS+numLHS + (value==1?1:0)*pow(2,tBit);
 		return numRHS+numLHS + (value==1?1:0)*(1<<tBit);
 	}
@@ -48,23 +71,55 @@ public:
 		return 0;
 	}
 public:
+	stringstream statusStream;
 	string status;
 	int msgCount;
 	string log;
 	QC(int init_qBits)
-		:log(""),status(""),msgCount(0)
+		:log(""),status(""),msgCount(0),normalization(1)
 	{
 		qBits=init_qBits;
 		amplitudes.resize((int)pow(2,qBits));
-		amplitudes[0]=(1,0); //Set all qbits to zero
+		amplitudes[0]=complex<decimal>(1,0); //Set all qbits to zero
 		status=statusPrefix()+"Initialized to simulate " + to_string(qBits) + " qBits. " + to_string((int)pow(2,qBits)) + " complex numbers reserved.";
 		log+=status+"\n";
 	}
-	int init_qBit(int qBit,int value)
+	void init_qBit(int qBit,int value)
 	{
+		statusStream.str("");
+		statusStream<<statusPrefix()<<"Initializing qBit: " << qBit << " to " << value << "\n";
+		for(int i=0;i< (1<<(qBits-1)) - 1; i++)
+		{
+			int survivorBasis=insertBit(i,qBit,value);
+			int killBasis=insertBit(i,qBit,!value);
+			complex<decimal> amplitude=amplitudes[survivorBasis];
+			complex<decimal> modifyAmplitude=amplitudes[killBasis];
+			if(!(amplitude.real()==0 && amplitude.imag()==0))
+			{
+				normalization -= norm(modifyAmplitude);
+				modifyAmplitude=0;
+				statusStream<< "Normalization: "<<normalization<<endl
+						<<	"Modified qBit: "<< printNumFancy(killBasis)<<endl
+						<<	"Survivor qBit: "<< printNumFancy(survivorBasis)<<endl<<"--"<<endl;
+			}
+			else
+			{
+				if(!(modifyAmplitude.real()==0 && modifyAmplitude.imag()==0))
+				{
+				amplitude = modifyAmplitude;
+				modifyAmplitude = 0;
+				//No need to change normalization, we just swapped
+				statusStream<<	"Modified basis: "<< printNumFancy(killBasis)<<endl
+						<<	"Survivor basis: "<< printNumFancy(survivorBasis)<<endl<<"--"<<endl;
 
-
+				}
+			}
+			
+		}
+		status=statusStream.str();
+		log+=status+"\n";
 	}
+	
 	//QC() : QC(10) {};
 };
 
@@ -73,12 +128,8 @@ void main()
 {
 	QC<float> qc(8);
 	cout<<qc.status<<endl;
-	bitset<8> n(string("00001110"));
-	//cout<<n.to_ullong()<<endl;
-	//int n=0xF;
-	int nDash=qc.insertBit(n.to_ulong(),0,1);
-	cout<<"So we had\n"<<n<<endl<<(bitset<8>) nDash<<endl<<"Did that work?";
-	//cout<<"This works";
+	qc.init_qBit(2,1);
+	cout<<qc.status<<endl;
 	_getch();
 
 }
